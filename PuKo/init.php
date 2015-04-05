6a482aad399cb58f79823947470ace20552107f1
Pushi <pushi95rus@gmail.com>
2015-04-05 13:11:45 +0300

    сделана форма авторизации, и настраиваевается главная страница

--- PuKo/init.php
+++ PuKo/init.php
@@ -1,2 +1,15 @@
-<?php
-include_once 'auth/init.php';
+﻿<?php
+
+/**
+ * Подключает функции, и модули, необходимые движку.
+ */
+
+include_once 'include.php';
+
+foreach ($BIN_ARR as $BIN){
+    include_once("bin/".$BIN.".php");
+}
+
+foreach ($MODULES_ARR as $MODULE){
+    include_once("module/".$MODULE."/init.php");
+}
--- PuKo/module/auth/init.php
+++ PuKo/module/auth/init.php
@@ -1,2 +1,15 @@
-<?php
-$auth;
+﻿<?php
+/**
+ * Модуль авторизации
+ */
+
+define(BAD_USERNAME, 1);
+define(AUTHORIZED, 0);
+
+/**
+ * Функция для авторизации пользователя. Возвращает BAD_USERNAME, если не найден
+ * такой пользователь или пароль и AUTHORIZED, если вход выполнен успешно
+ */
+function authorize($__username, $__password){
+    return AUTHORIZED;
+}
